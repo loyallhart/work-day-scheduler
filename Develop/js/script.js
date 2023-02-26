5c7d2af657cc4c012ead3d683f4a45002bd4e7ec
hart <100047482+loyallhart@users.noreply.github.com>
2023-02-26 03:13:17 -0500

    added js

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,12 +1,50 @@
 // Dayjs date
-
-
-
+let date = document.getElementById("currentDay")
+date.innerHTML = dayjs().format('dddd, MMMM D, YYYY')
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+$(document).ready(function () {
+
+    $('.saveBtn').on('click', function () {
+        let text = $(this).siblings(".description").val();
+        let time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+
+    function timeTracker() {
+        let timeNow = dayjs().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            let blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // To check the time and add the classes for background indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+
+            }
+        })
+    }
+
+
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -25,4 +63,17 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
+
+    // Get item from local storage if any
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    timeTracker();
+});
